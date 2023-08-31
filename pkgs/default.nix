41e46c5cb909fffa08d69f8cc820089632f4e7c0
ShaddyDC <shaddythefirst@gmail.com>
2023-08-31 15:33:00 +0200

    feat: Add collada2gltf

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -3,6 +3,11 @@
 
   perSystem = {pkgs, ...}: {
     packages = {
+      collada2gltf = pkgs.callPackage ./collada2gltf {
+        inherit pkgs;
+        inherit (pkgs.stdenv) mkDerivation;
+      };
+
       openusd = pkgs.python3.pkgs.openusd;
     };
   };
